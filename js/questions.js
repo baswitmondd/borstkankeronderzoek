0ebff6e41dce7a6964eefffe6c26a7dd2d4ea77b
baswitmondd <bas.witmond@gmail.com>
2026-09-25 14:00:38 +0200

    iPad-formaat (810x1080, geen scroll), pijnschaal per taal, icoon-plaatsing per vraag

--- js/questions.js
+++ js/questions.js
@@ -26,8 +26,8 @@ const QUESTIONS = [
     backSound: { x: 0, y: 14 },
     textPos: "bottom",
     icon: { size: 44, top: -12, left: 67, c1: "#DF897F", c2: "#903F2F", c3: "#BE6964" },
-    frontIcon: { size: 95, top: -12, left: 25 },
-    backIcon: { size: 95, top: -12, left: 25 },
+    frontIcon: { size: 95, top: -15, left: 30 },
+    backIcon: { size: 95, top: -15, left: 30 },
     front: {
       title: "Het bevolkingsonderzoek kan borstkanker vroeg ontdekken.",
       text: "Het vroeg ontdekken van borstkanker vergroot de kans op een succesvolle behandeling. En als borstkanker vroeg wordt ontdekt, hoeft de behandeling vaak ook minder zwaar te zijn."
@@ -49,8 +49,8 @@ const QUESTIONS = [
     backSound: { x: 0, y: 14 },
     textPos: "center",
     icon: { size: 50.8, top: 66.2, left: -14.0, c1: "#FFD9CB", c2: "#F7A58D", c3: "#DF897F", c3Inset: 43 },
-    frontIcon: { size: 95, top: -12, left: 25 },
-    backIcon: { size: 95, top: -12, left: 25 },
+    frontIcon: { size: 40, top: -7, left: 68 },
+    backIcon: { size: 40, top: -7, left: 68 },
     front: {
       title: "Het samendrukken van de borsten kan vervelend of pijnlijk zijn.",
       text: "Dat verschilt per persoon. Gemiddeld geven de deelnemers op een schaal van 0 (geen pijn) tot 10 (ergst denkbare pijn) <strong>een score van 4</strong>."
@@ -72,8 +72,8 @@ const QUESTIONS = [
     backSound: { x: 0, y: 14 },
     textPos: "bottom",
     icon: { size: 61.8, top: -8.3, left: 48.5, c1: "#6E2711", c2: "#9C3829", c3: "#C65F55", c3Inset: 45 },
-    frontIcon: { size: 95, top: -12, left: 25 },
-    backIcon: { size: 95, top: -12, left: 25 },
+    frontIcon: { size: 95, top: -15, left: 30 },
+    backIcon: { size: 95, top: -15, left: 30 },
     front: {
       title: "De meeste afwijkingen worden gevonden.",
       text: "Elke foto wordt door 2 artsen bekeken. Zij kijken onafhankelijk van elkaar. Zo is de kans groter dat borstkanker wordt gevonden."
@@ -95,8 +95,8 @@ const QUESTIONS = [
     backSound: { x: 0, y: 14 },
     textPos: "bottom",
     icon: { size: 42.9, top: 72.2, left: -15.65, c1: "#6E2711", c2: "#500E0B", c3: "#D38D82" },
-    frontIcon: { size: 68, top: -15, left: 52 },
-    backIcon: { size: 68, top: -15, left: 52 },
+    frontIcon: { size: 68, top: -17, left: 57 },
+    backIcon: { size: 68, top: -17, left: 57 },
     front: {
       title: "De meeste vrouwen krijgen een gunstige uitslag.",
       text: "Van elke 100 vrouwen krijgen er 98 de uitslag 'geen afwijking'. Dat kan een gevoel van rust geven."
@@ -118,8 +118,8 @@ const QUESTIONS = [
     backSound: { x: 0, y: 14 },
     textPos: "center",
     icon: { size: 62.6, top: 59.0, left: 50.8, c1: "#9C3829", c2: "#CB6361", c3: "#EBA992" },
-    frontIcon: { size: 95, top: -12, left: 25 },
-    backIcon: { size: 95, top: -12, left: 25 },
+    frontIcon: { size: 95, top: -15, left: 30 },
+    backIcon: { size: 95, top: -15, left: 30 },
     front: {
       title: "Een verwijzing kan zorgen en spanning geven.",
       text: "Na een verwijzing is extra onderzoek nodig om zeker te weten of er iets aan de hand is. Het wachten op die uitslag kan spannend zijn."
@@ -141,8 +141,8 @@ const QUESTIONS = [
     backSound: { x: 0, y: 14 },
     textPos: "bottom",
     icon: { size: 54.5, top: -20, left: 30, c1: "#F7A58D", c2: "#B37B62", c3: "#DF897F", c3Inset: 41 },
-    frontIcon: { size: 95, top: -12, left: 25 },
-    backIcon: { size: 85, top: -12, left: 25 },
+    frontIcon: { size: 95, top: -18, left: 36 },
+    backIcon: { size: 85, top: -18, left: 36 },
     front: {
       title: "Borstkanker wordt bijna altijd behandeld.",
       text: "Je overlegt hierover samen met je arts. Als borstkanker vroeg wordt behandeld, kan dat levens redden."
@@ -164,8 +164,8 @@ const QUESTIONS = [
     backSound: { x: 0, y: 14 },
     textPos: "bottom",
     icon: { size: 60.5, top: 55.6, left: 47.2, c1: "#F3B5A8", c2: "#B37B62", c3: "#DF897F", c3Inset: 43 },
-    frontIcon: { size: 68, top: -15, left: 52 },
-    backIcon: { size: 68, top: -15, left: 52 },
+    frontIcon: { size: 68, top: -17, left: 57 },
+    backIcon: { size: 68, top: -17, left: 57 },
     front: {
       title: "Meedoen aan het bevolkingsonderzoek is gratis.",
       text: "De overheid betaalt het onderzoek. Daarom hoef je zelf niets te betalen."
@@ -187,8 +187,8 @@ const QUESTIONS = [
     backSound: { x: 0, y: 14 },
     textPos: "center",
     icon: { size: 52.5, top: 60.9, left: 60.8, c1: "#6E2711", c2: "#C9625F", c3: "#DF897F" },
-    frontIcon: { size: 65, top: -14, left: 55 },
-    backIcon: { size: 65, top: -14, left: 55 },
+    frontIcon: { size: 85, top: -21, left: 43 },
+    backIcon: { size: 85, top: -21, left: 43 },
     front: {
       title: "Het bevolkingsonderzoek is bedoeld voor vrouwen zonder veranderingen of klachten aan hun borsten.",
       text: "Heb je geen klachten? Dan kun je meedoen. Meedoen is altijd je eigen keuze."
@@ -210,8 +210,8 @@ const QUESTIONS = [
     backSound: { x: 0, y: 14 },
     textPos: "bottom",
     icon: { size: 63.1, top: -18, left: 55, c1: "#DF897F", c2: "#6E2711", c3: "#500E0B", c3Inset: 43 },
-    frontIcon: { size: 95, top: -22, left: 35 },
-    backIcon: { size: 95, top: -22, left: 35 },
+    frontIcon: { size: 85, top: -21, left: 43 },
+    backIcon: { size: 85, top: -21, left: 43 },
     front: {
       title: "Vrouwen van 50 tot en met 75 jaar krijgen een uitnodiging voor het bevolkingsonderzoek borstkanker.",
       text: "Vanaf je 50e krijg je vanzelf een uitnodiging van Bevolkingsonderzoek Nederland. Daarna krijg je elke 2 tot 3 jaar opnieuw een uitnodiging, tot en met je 75e. Alle informatie staat in de brief en folder die je ontvangt."
